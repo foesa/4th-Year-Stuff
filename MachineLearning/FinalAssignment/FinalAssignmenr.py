36fe191c541e0a5956f5f4b36afc5dd4c4284ba2
Efeosa Eguavoen <33104551+foesa@users.noreply.github.com>
2021-01-20 02:11:44 +0000

    trying to get translations working

--- MachineLearning/FinalAssignment/FinalAssignmenr.py
+++ MachineLearning/FinalAssignment/FinalAssignmenr.py
@@ -1,36 +1,76 @@
 import json
 import pandas as pd
 from googletrans import Translator
+from google_trans_new import google_translator
+import os.path
+from os import path
 
-def read_file():
-    X = []
-    Y = []
-    Z = []
-    with open('reviews_262.jl.txt', encoding='utf8') as json_file:
-        translator = Translator()
-        for i in json_file:
-            data = json.loads(i)
-            if isEnglish(data['text']):
-                X.append(data['text'])
+
+def read_file(text=None):
+    if text is None:
+        Y = []
+        Z = []
+        with open('reviews_262.jl.txt', encoding='utf8') as json_file:
+            text_vals = []
+            for i in json_file:
+                data = json.loads(i)
+                text_vals.append(data['text'])
                 Y.append(data['voted_up'])
                 Z.append(data['early_access'])
-            else:
-                translation = translator.translate(data['text'])
-                print(translation)
+            return text_vals, Y, Z
+    translations = []
+    origins = []
+    with open(text) as json_file:
+        for i in json_file:
+            data = json.loads(i)
+            translations.append(data['text'])
+            origins.append(data['origin'])
+    return translations, origins
 
-    print(len(X))
-    dataFrame = pd.DataFrame(data={'Text': X, 'Voted Up': Y, 'Early Access': Z})
-    print(dataFrame)
-    return dataFrame
+
+def translate_text(text):
+    translator = Translator()
+    translation = translator.translate(text)
+    origin = text
+    translated = translation
+    return origin, translated
 
 
-def isEnglish(text) -> bool:
-    try:
-        text.encode(encoding='utf-8').decode('ascii')
-    except UnicodeDecodeError:
-        return False
+def write_translations(texts, origins):
+    with open('translations.txt', 'w') as f:
+        for text, origin in zip(texts, origins):
+            f.write('{"text" : ' + '"' + text + '"' + '}' + '{"origin" : ' + '"' + origin + '"' + '}' + '\n')
+
+
+def main():
+    texts, Y, Z = read_file()
+    origin = []
+    translated = []
+    if path.exists('translations.txt'):
+        translated, origin = read_file('translations.txt')
     else:
-        return True
+        i = 0
+        # chunks = [texts[x:x + 100] for x in range(0, len(texts), 100)]
+        # for chunk in chunks:
+        #     part_origin, part_translated = translate_text(chunk)
+        #     print(part_translated)
+        #     print(i)
+        #     i = i +1
+        for text in texts:
+            part_origin, part_translated = translate_text(text)
+            origin.append(part_origin)
+            translated.append(part_translated)
+            print(i, part_translated)
+            i = i + 1
+        write_translations(translated, origin)
+    data = pd.DataFrame({'Origin': origin,
+                         'Translated': translated,
+                         'Voted Up': Y,
+                         'Early Access': Z})
+    print(data)
 
+
+if __name__ == '__main__':
+    main()
 
 read_file()
